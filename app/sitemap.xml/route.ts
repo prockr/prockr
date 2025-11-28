dff956797b438297d23bee519d38bcabbbe7e83f
pestjeddah <your-email@example.com>
2025-11-28 19:11:56 +0200

    Fix sitemap.xml - remove XSL stylesheet reference that caused blank page

--- app/sitemap.xml/route.ts
+++ app/sitemap.xml/route.ts
@@ -25,7 +25,6 @@ export async function GET() {
     .join('\n');
   
   const xml = `<?xml version="1.0" encoding="UTF-8"?>
-<?xml-stylesheet type="text/xsl" href="/sitemap-style.xsl"?>
 <sitemapindex xmlns="http://www.sitemaps.org/schemas/sitemap/0.9">
 ${sitemapEntries}
 </sitemapindex>`;
